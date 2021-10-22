3e7a34c2ef2d65e66192cdac6ecc6c5f1d41624a
tripulse <poli.sb@protonmail.com>
2021-10-23 00:17:40 +0530

    prototype of a simple player

--- src/main/java/bittu/Bot.java
+++ src/main/java/bittu/Bot.java
@@ -4,22 +4,48 @@ import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class Bot extends ListenerAdapter {
-	@Override
-	public void onSlashCommand(SlashCommandEvent event) {
-		switch(event.getCommandPath()) {
-		case "join" -> { 
-			var vc = event.getMember().getVoiceState().getChannel();
-			var manager = event.getGuild().getAudioManager();
-			
+	private static int joinInvokerChannel(SlashCommandEvent event) {
+		var vc = event.getMember().getVoiceState().getChannel();
+		if(vc == null) {
+			event.reply("age thik kore voice chenle join kora shek").queue();
+			return -1;
+		}
+		
+		var manager = event.getGuild().getAudioManager();
+		
+		if(manager.getConnectedChannel() == null) {
 			manager.openAudioConnection(vc);
 			manager.setSelfDeafened(true);
-			manager.setSendingHandler(new AudioPlayer());
-			
+			manager.setSendingHandler(new AudioPlayerWrapper());
+		
 			event.reply("Joined " + vc.getAsMention()).queue();
+		} else {
+			event.reply("Already joined" + vc.getAsMention()).queue();
+		}
+		
+		return 0;
+	}
+	
+	@Override
+	public void onSlashCommand(SlashCommandEvent event) {
+		switch(event.getCommandPath()) {		
+		case "play" -> {
+			if(joinInvokerChannel(event) == -1)
+				return; // ensure we've joined a voice channel.
+			
+			var player = (AudioPlayerWrapper)event.getGuild().getAudioManager().getSendingHandler();
+			
+			try {
+				player.stream(event.getOption("url").getAsString());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		
 		case "leave" -> {
-			var vc = event.getMember().getVoiceState().getChannel();
+			var vc = event.getGuild().getAudioManager().getConnectedChannel();
+			if(vc == null)
+				event.reply("age thik kore chok diye dekha shek").queue();
 			
 			event.getGuild().getAudioManager().closeAudioConnection();
 			event.reply("Left " + vc.getAsMention()).queue();
--- src/main/java/bittu/Main.java
+++ src/main/java/bittu/Main.java
@@ -3,11 +3,13 @@ package bittu;
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 public class Main {
 	public static void main(String[] args) throws LoginException, InterruptedException {
+		// load JNI library for music decoding.
 		var bot = JDABuilder
 				.createDefault(args[0])
 				.setBulkDeleteSplittingEnabled(false)
@@ -22,6 +24,8 @@ public class Main {
 			.updateCommands()
 			.addCommands(
 					new CommandData("join", "Summon in your current VC"),
+					new CommandData("play", "Play an URL")
+						.addOption(OptionType.STRING, "url", "URL to be played", true),
 					new CommandData("leave", "Leave from your current VC")
 			).queue();
 	}
